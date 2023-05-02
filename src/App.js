ff226e4d9083719c460e22016f1c9847a2ee1cba
ManausK <1905246@kiit.ac.in>
2023-05-02 17:58:02 +0530

    Height, Grid, useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React, { useState, useEffect } from "react";
 import FlashcardList from "./FlashcardList";
 import "./app.css";
+import axios from "axios";
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
-  return <FlashcardList flashcards={flashcards} />;
+
+  useEffect(() => {
+    axios.get("https://opentdb.com/api.php?amount=10").then((res) => {
+      setFlashcards(
+        res.data.results.map((questionItem, index) => {
+          const answer = decodeString(questionItem.correct_answer);
+          const options = [
+            ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+            answer,
+          ];
+          return {
+            id: `${index}-${Date.now}`,
+            question: decodeString(questionItem.question),
+            answer: answer,
+            options: options.sort(() => Math.random() - 0.5),
+          };
+        })
+      );
+    });
+  }, []);
+
+  function decodeString(str) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
+
+  return (
+    <div className="container">
+      <FlashcardList flashcards={flashcards} />
+    </div>
+  );
 }
 
 const SAMPLE_FLASHCARDS = [
